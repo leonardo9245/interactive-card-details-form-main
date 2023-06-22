a8ca2c5aea61f7b0c4b2be0b0db15a68384c1c73
leonardo9245 <leonardo92reis@gmail.com>
2023-06-22 16:20:44 -0300

    feat: error message

--- script.js
+++ script.js
@@ -2,12 +2,37 @@ const inputs = document.querySelectorAll('input');
 const cardInfos = document.querySelectorAll(
   '.card-name, .card-number, .card-month, .card-year, .card-cvc'
 );
+const errorElement = document.querySelectorAll('.error-message');
+
+const checkInputs = (id, value) => {
+  const element = document.getElementById(id);
+
+  errorElement.forEach(e => {
+    if (value === '') {
+      element.classList.add('error-input');
+      if (e.classList.contains(id)) {
+        e.classList.add('display');
+      }
+    } else {
+      element.classList.remove('error-input');
+      e.classList.remove('display');
+    }
+  });
+};
+
+const handleButton = event => {
+  event.preventDefault();
+  for (let input of inputs) {
+    checkInputs(input.id, input.value);
+  }
+};
 
 for (let input of inputs) {
   for (let infos of cardInfos) {
     if (input.id === infos.classList.value) {
       input.addEventListener('input', () => {
         infos.innerHTML = `${input.value}`;
+        input.value = `${input.value}`;
       });
     }
   }
